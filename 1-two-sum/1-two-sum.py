ea80ecf1cb9a649b9b41baba214f39a44d482d4a
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2022-07-11 15:58:30 +0200

    Time: 111 ms (50.99%), Space: 15.2 MB (50.37%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        #Regular methode
         lg=len(nums)
         for i in range(lg-1):
             for j in range(i+1, lg):
                 if nums[i]+nums[j]==target:
                     return [i,j]
+        """
+        #Hash map methode
+        seen={}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
+            if remaining in seen:
+                return [i, seen[remaining]]
+            seen[value]=i
+        
