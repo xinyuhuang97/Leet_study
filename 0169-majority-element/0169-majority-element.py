3085d593451f46b11f12870a5fc334568d616192
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-02-23 18:08:56 +0100

    Time: 183 ms (7.61%), Space: 18 MB (83.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -6,13 +6,5 @@ class Solution:
                 count[nums[i]]=1
             else:
                 count[nums[i]]+=1
-        """biggest_occurence=0
-        number=0
-        
-        for keys in count:
-            if count[keys]>biggest_occurence:
-                biggest_occurence=count[keys]
-                number=keys
-                """
         number=max(count,key=count.get)
         return number
