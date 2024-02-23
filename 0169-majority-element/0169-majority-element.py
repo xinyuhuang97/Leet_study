4117835ba065e67ddca7f7df5820db81b5331785
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-02-23 18:05:51 +0100

    Time: 190 ms (6.58%), Space: 18.1 MB (27.18%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count={}
+        for i in range(len(nums)):
+            if nums[i] not in count:
+                count[nums[i]]=1
+            else:
+                count[nums[i]]+=1
+        biggest_occurence=0
+        number=0
+        for keys in count:
+            if count[keys]>biggest_occurence:
+                biggest_occurence=count[keys]
+                number=keys
+        return number
