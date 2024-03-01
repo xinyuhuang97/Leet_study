36000bfabd5ad257db9631c00e26ed701842f8a4
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-01 02:51:17 +0100

    Time: 60 ms (47.23%), Space: 19.6 MB (5.55%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,18 @@
-from typing import List
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        nb = len(prices)
-        if nb <= 1:
-            return 0
+        dp = [[0, 0] for _ in range(len(prices))]
 
-        max_profit = 0
+        # Base case: no stocks held on day 0
+        dp[0][0] = 0
+        # Buying a stock on day 0, so the profit is negative of the stock price
+        dp[0][1] = -prices[0]
 
-        for i in range(1, nb):
-            # If the price today is higher than yesterday, we can make a profit
-            if prices[i] > prices[i - 1]:
-                max_profit += prices[i] - prices[i - 1]
+        for i in range(1, len(prices)):
+            # Calculate the maximum profit on day i with no stocks held
+            dp[i][0] = max(dp[i - 1][0], prices[i] + dp[i - 1][1])
+            # Calculate the maximum profit on day i with stocks held
+            dp[i][1] = max(dp[i - 1][1], dp[i][0] - prices[i])
 
-        return max_profit
-
-
+        # The maximum profit is either selling the stock on the last day or not holding any stocks
+        return max(dp[-1][0], 0)
 
