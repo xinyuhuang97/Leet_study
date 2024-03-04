aab0c74eee119242920c6e6180e5092f9ddf4c20
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-04 02:21:17 +0100

    Time: 53 ms (23.74%), Space: 16.6 MB (78.54%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -5,18 +5,14 @@ class Solution:
             return dic[s[0]]
         c_before=dic[s[1]]
         c_after=0
-        value=0
-        bool_substraction=0
-        
+        value=0        
         for i in range(len(s)-1):
             c_before=dic[s[i]]
             c_after=dic[s[i+1]]
             if c_before<c_after:
                 value+=-c_before
-                #bool_substraction=1
             else:
                 value+=c_before
-            print("v",value)
             c_before=c_after
         value+=c_before
         return value
