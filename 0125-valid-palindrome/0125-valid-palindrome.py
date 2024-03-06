df85a90fefd98612534eae257e303c0ff3b6ec32
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-06 01:17:04 +0100

    Time: 42 ms (74.00%), Space: 22.1 MB (8.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,11 +2,10 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         #"A" - "Z" varie de 65 à 90 et celle de "a" - "z" de 97 à 122.
         s=s.replace(" ","")      
-        list_s="".join(i.lower() for i in s if i.isalnum())
-        lg=len(list_s)
-        lg_half=int(lg/2)
-        for i in range(lg_half):
-            if list_s[i]!=list_s[lg-i-1]:
+        s="".join(i.lower() for i in s if i.isalnum())
+        lg=len(s)
+        for i in range(int(lg/2)):
+            if s[i]!=s[lg-i-1]:
                 return False
         return True
         
