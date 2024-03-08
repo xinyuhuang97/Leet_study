da3b7d916649145c6b52ed43eb282fba5eb015ff
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-08 23:55:26 +0100

    Time: 88 ms (92.41%), Space: 16.7 MB (51.28%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,27 +1,27 @@
 import collections
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n = len(board)
+        n=len(board)
         def label_to_position(label):
-            r, c = divmod(label-1, n)
-            if r % 2 == 0:
-                return n-1-r, c
+            r = (label-1)//n
+            if r%2==0:
+                c = (label-1)%n
             else:
-                return n-1-r, n-1-c
-            
-        seen = set()
-        queue = collections.deque()
-        queue.append((1, 0))
-        while queue:
-            label, step = queue.popleft()
-            r, c = label_to_position(label)
-            if board[r][c] != -1:
-                label = board[r][c]
-            if label == n*n:
-                return step
-            for x in range(1, 7):
-                new_label = label + x
-                if new_label <= n*n and new_label not in seen:
-                    seen.add(new_label)
-                    queue.append((new_label, step+1))
+                c = (n-1)-(label-1)%n
+            return (n-1)-r, c
+        
+        visit, q = set([1]), collections.deque([(1,0)])
+        
+        while q:
+            label, count = q.popleft()
+            r,c =label_to_position(label)
+            if board[r][c]!=-1:
+                label=board[r][c]
+            if label==n*n:
+                return count     
+            for step in range(1, 7):
+                new_label=label+step
+                if new_label<=n*n and new_label not in visit:
+                    q.append( (new_label, count+1))
+                    visit.add(new_label)
         return -1
