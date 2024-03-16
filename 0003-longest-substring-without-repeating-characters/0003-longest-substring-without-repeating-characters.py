5c1b0a4c8b71a35efd395dd90433a407ad4b39c7
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-16 17:20:48 +0100

    Time: 82 ms (20.25%), Space: 16.7 MB (63.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,18 +5,14 @@ class Solution:
             return lg
         l,list_c,current_s,ans=0,[],'',0
         for r,letter in enumerate(s):
-            print(current_s, list_c)
-            if letter not in list_c:
-                
-                current_s+=letter
-                ans=max(ans,len(current_s))
+            if letter not in list_c:                 
                 list_c.append(letter)
             else:
                 while letter in list_c:
-                    ans=max(ans,len(current_s))
                     list_c.remove(current_s[0])
                     current_s=current_s[1:]
                 list_c.append(letter)
-                current_s+=letter
+            current_s+=letter
+            ans=max(ans,len(current_s))
         return ans
                     
