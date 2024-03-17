e3b2f2d24b97f3e91464a310212ec260e10a5896
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-18 00:37:34 +0100

    Time: 28 ms (91.84%), Space: 16.5 MB (90.65%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,31 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        range_list=[]
+        if nums==[]:
+            return []
+        if len(nums)==1:
+            return [str(nums[0])]
+        first_number=nums[0]
+        last_number=nums[0]
+        for i,num in enumerate(nums):
+            print(num)
+            if i==0:
+                continue
+            
+                
+            if last_number+1==num:
+                last_number+=1
+            else:
+                if  first_number==last_number:
+                    range_list.append(str(first_number))
+                else:
+                    range_list.append(str(first_number)+"->"+str(last_number))
+                first_number=num
+                last_number=num
+            if  i==len(nums)-1:
+                if  first_number==last_number:
+                    range_list.append(str(first_number))
+                else:
+                    range_list.append(str(first_number)+"->"+str(last_number))
+        return range_list
+            
