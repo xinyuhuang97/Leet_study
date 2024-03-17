9212d3fdee53a7f1822dc624aa835443795155a5
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-18 00:39:00 +0100

    Time: 27 ms (93.52%), Space: 16.5 MB (48.75%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -8,11 +8,8 @@ class Solution:
         first_number=nums[0]
         last_number=nums[0]
         for i,num in enumerate(nums):
-            print(num)
             if i==0:
-                continue
-            
-                
+                continue  
             if last_number+1==num:
                 last_number+=1
             else:
@@ -22,10 +19,9 @@ class Solution:
                     range_list.append(str(first_number)+"->"+str(last_number))
                 first_number=num
                 last_number=num
-            if  i==len(nums)-1:
-                if  first_number==last_number:
-                    range_list.append(str(first_number))
-                else:
-                    range_list.append(str(first_number)+"->"+str(last_number))
+        if  first_number==last_number:
+            range_list.append(str(first_number))
+        else:
+            range_list.append(str(first_number)+"->"+str(last_number))
         return range_list
             
