639a64d093d5b55502cfb971f39a29e8d17f4250
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-17 01:54:38 +0100

    Time: 1036 ms (73.22%), Space: 17 MB (47.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -4,15 +4,15 @@ class Solution:
         if n==0:
             return 0
         dp=[1]*n
-        lgest=1
-        for i in range(1,n):
-            
+        #lgest=1
+        for i in range(1,n):          
             for j in range(i):
                 #print(nums[i],nums[j])
                 #print(dp,dp[j]+1)
                 if nums[j]<nums[i] and dp[j]+1>dp[i]:
                     dp[i]=dp[j]+1
                     #print(nb)
-            lgest=max(lgest,dp[i])
-        return lgest
+            #lgest=max(lgest,dp[i])
+        #return lgest
+        return max(dp)
         
