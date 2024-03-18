a805837c8ebdb48a76639eb2e427285895a1df81
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-18 02:29:00 +0100

    Time: 31 ms (81.24%), Space: 16.6 MB (17.53%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -13,7 +13,6 @@ class Solution:
         while deque:
             info=deque.popleft()
             gene,count=info
-            #print("gene",gene)
             for i,letter in enumerate(gene):
                 for new_letter in mut_dic:
                     if new_letter==gene[i]:
@@ -22,14 +21,10 @@ class Solution:
                         mut=new_letter+gene[1:]
                     else:
                         mut=gene[:i]+new_letter+gene[i+1:]
-                    #print(i, mut)
-                    #print(mut, endGene, mut==endGene)
                     if mut==endGene:
-                        print("hi")
                         return count+1
                     if mut not in solutions and mut in bank:
                         deque.append((mut,count+1))
                         solutions.append(mut)
-        #print(solutions)
         return -1
         
