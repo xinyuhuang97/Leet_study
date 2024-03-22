19ecec42decd5f9251f23611b8a539360578184b
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-22 14:30:38 +0100

    Time: 56 ms (47.07%), Space: 16.6 MB (17.20%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,7 +2,6 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         string_x=list(str(x))
         n=len(string_x)
-        #print(string_x)
         for i in range(n//2):
             if string_x[i]!=string_x[n-i-1]:
                 return False
