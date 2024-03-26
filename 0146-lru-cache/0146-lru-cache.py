9f2fef3aee1d7c0b4f3b8adb6e1e853363a2ca6f
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-26 04:14:14 +0100

    Time: 8918 ms (5.01%), Space: 77.4 MB (78.99%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -46,41 +46,12 @@ class LRUCache:
             self.del_node(cur)
         if self.capacity==len(self.m):
             del self.m[self.tail.prev.key]
-            self.del_node(self.tail.prev)
-            
+            self.del_node(self.tail.prev) 
         self.add_node(self.Node(key, value))
         self.m[key]=self.head.next
             
         
-"""
-    def __init__(self, capacity: int):
-        self.capacity=capacity
-        self.cache=collections.deque([])
-        self.value_key={}
-    def get(self, key: int) -> int:
-        #print(key,self.cache)
-        if key in self.value_key.keys():
-            self.cache.remove(key)
-            self.cache.append(key)
-            return self.value_key[key]
-        return -1
 
-    def put(self, key: int, value: int) -> None:
-        #print(key, value)
-        #print(self.cache)
-        if key in self.value_key.keys():
-            self.cache.remove(key)
-            self.cache.append(key)
-            self.value_key[key]=value
-        else:
-            if self.capacity==len(self.cache):
-                delete_cache=self.cache.popleft()
-                self.value_key.pop(delete_cache,None)
-                #print(self.cache)
-            self.cache.append(key)
-            self.value_key[key]=value
-            
-"""
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
