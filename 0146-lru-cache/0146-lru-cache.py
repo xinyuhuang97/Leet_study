4c041e441099253a6bc551fd6f8a1f2484096dd1
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-26 04:15:04 +0100

    Time: 605 ms (23.04%), Space: 78.5 MB (6.41%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,55 +1,55 @@
-import collections
 class LRUCache:
     class Node:
-        def __init__(self, key, value):
-            self.prev=None
-            self.next=None
-            self.key=key
-            self.value=value
+        def __init__(self, key, val):
+            self.key = key
+            self.val = val
+            self.prev = None
+            self.next = None
+
     def __init__(self, capacity: int):
-        self.capacity=capacity
-        self.head=self.Node(-1,-1)
-        self.tail=self.Node(-1,-1)
-        self.head.next=self.tail
-        self.tail.prev=self.head
-        self.m={}
-    def add_node(self, node):
-        tmp=self.head.next
-        self.head.next=node
-        node.prev=self.head
-        node.next=tmp
-        tmp.prev=node
-        
-    def del_node(self, node):
-        node.next.prev=node.prev
-        node.prev.next=node.next
-        
-        
+        self.cap = capacity
+        self.head = self.Node(-1, -1)
+        self.tail = self.Node(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.m = {}
+
+    def addNode(self, newnode):
+        temp = self.head.next
+        newnode.next = temp
+        newnode.prev = self.head
+        self.head.next = newnode
+        temp.prev = newnode
+
+    def deleteNode(self, delnode):
+        prevv = delnode.prev
+        nextt = delnode.next
+        prevv.next = nextt
+        nextt.prev = prevv
+
     def get(self, key: int) -> int:
-        if key in self.m.keys():
-            ans=self.m[key].value
-            node=self.m[key]
+        if key in self.m:
+            resNode = self.m[key]
+            ans = resNode.val
             del self.m[key]
-            self.del_node(node)
-            self.add_node(node)
-            self.m[key]=self.head.next
+            self.deleteNode(resNode)
+            self.addNode(resNode)
+            self.m[key] = self.head.next
             return ans
         return -1
-            
+
     def put(self, key: int, value: int) -> None:
-        head=self.head
-        while head!=None:
-            head=head.next
-        if key in self.m.keys():
-            cur=self.m[key]
+        if key in self.m:
+            curr = self.m[key]
             del self.m[key]
-            self.del_node(cur)
-        if self.capacity==len(self.m):
+            self.deleteNode(curr)
+
+        if len(self.m) == self.cap:
             del self.m[self.tail.prev.key]
-            self.del_node(self.tail.prev) 
-        self.add_node(self.Node(key, value))
-        self.m[key]=self.head.next
-            
+            self.deleteNode(self.tail.prev)
+
+        self.addNode(self.Node(key, value))
+        self.m[key] = self.head.next
         
 
 
