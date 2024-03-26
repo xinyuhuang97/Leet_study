29e26f8cbf00c6d3c064bff9d157da578a0e880b
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-03-26 04:13:33 +0100

    Time: 9116 ms (5.01%), Space: 78.9 MB (6.41%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,6 +1,58 @@
 import collections
 class LRUCache:
-
+    class Node:
+        def __init__(self, key, value):
+            self.prev=None
+            self.next=None
+            self.key=key
+            self.value=value
+    def __init__(self, capacity: int):
+        self.capacity=capacity
+        self.head=self.Node(-1,-1)
+        self.tail=self.Node(-1,-1)
+        self.head.next=self.tail
+        self.tail.prev=self.head
+        self.m={}
+    def add_node(self, node):
+        tmp=self.head.next
+        self.head.next=node
+        node.prev=self.head
+        node.next=tmp
+        tmp.prev=node
+        
+    def del_node(self, node):
+        node.next.prev=node.prev
+        node.prev.next=node.next
+        
+        
+    def get(self, key: int) -> int:
+        if key in self.m.keys():
+            ans=self.m[key].value
+            node=self.m[key]
+            del self.m[key]
+            self.del_node(node)
+            self.add_node(node)
+            self.m[key]=self.head.next
+            return ans
+        return -1
+            
+    def put(self, key: int, value: int) -> None:
+        head=self.head
+        while head!=None:
+            head=head.next
+        if key in self.m.keys():
+            cur=self.m[key]
+            del self.m[key]
+            self.del_node(cur)
+        if self.capacity==len(self.m):
+            del self.m[self.tail.prev.key]
+            self.del_node(self.tail.prev)
+            
+        self.add_node(self.Node(key, value))
+        self.m[key]=self.head.next
+            
+        
+"""
     def __init__(self, capacity: int):
         self.capacity=capacity
         self.cache=collections.deque([])
@@ -28,7 +80,7 @@ class LRUCache:
             self.cache.append(key)
             self.value_key[key]=value
             
-
+"""
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
