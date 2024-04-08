510100097e7cdd35e2282c652872122db2f79279
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-04-08 04:32:01 +0200

    Time: 50 ms (6.26%), Space: 16.8 MB (10.80%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,23 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack=[]
+        last_dir=""
+        for i, s in enumerate(path):
+            if s!="/":
+                last_dir+=s
+            if s=="/" or i==len(path)-1 :
+                print(last_dir)
+                if last_dir==".":
+                    last_dir=''
+                    continue
+                elif last_dir=="..":
+                    if stack!=[]:
+                        stack.pop()
+                else:
+                    if last_dir!="":
+                        stack.append(last_dir)
+                last_dir=''
+        if last_dir!='':
+            stack.append(last_dir)
+        print(stack)
+        return "/"+"/".join(stack)
