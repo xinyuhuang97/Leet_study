2c16915fff4e7966c34f56662b3262b7a69d76b0
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-04-17 23:21:48 +0200

    Time: 34 ms (72.89%), Space: 16.6 MB (66.22%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,7 +2,6 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         #return int(bin(n)[2:].zfill(32)[::-1],2)
         result = 0
-        print(n)
         for i in range(32):
             lsb = n&1
             result = result<<1
