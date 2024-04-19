003c780cf0ce978332f41f1397bc972940a86354
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-04-19 04:37:36 +0200

    Time: 37 ms (71.91%), Space: 16.5 MB (42.02%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -4,7 +4,7 @@ class Solution:
             return 0
         if x<4:
             return 1
-        val=2
-        while x//(val*val)>=1:
-            val+=1
-        return val-1
+        xn=x
+        while xn**2 > x:
+            xn = (xn+x//xn)//2
+        return xn
