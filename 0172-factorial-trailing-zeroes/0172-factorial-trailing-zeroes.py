10fd20b72ced206c17c84ae7d150b1bbe5a378cc
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-04-19 04:50:14 +0200

    Time: 39 ms (49.93%), Space: 16.5 MB (88.47%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,10 +1,8 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         cpt=0
-        sum_val=1
-        for i in range(1,n+1):
-            sum_val*=i
-        while sum_val%10==0:
-            sum_val=sum_val//10
-            cpt+=1
+        value=5
+        while n//value>=1:
+            cpt+=n//value
+            value*=5
         return cpt
