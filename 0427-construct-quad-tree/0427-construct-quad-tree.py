e75469f3da55cca18f337d27230effb15e930fe5
xinyuhuang97 <45275266+xinyuhuang97@users.noreply.github.com>
2024-04-20 04:05:58 +0200

    Time: 88 ms (73.74%), Space: 17.3 MB (62.11%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        size = len(grid)
+        if size == 1:
+            return Node(grid[0][0], True, None, None, None, None)
+        same_val = all(grid[i][j] == grid[0][0] for i in range(size) for j in range(size))
+        if same_val:
+            return Node(grid[0][0], True, None, None, None, None)
+        left_size = size // 2
+        topleft = self.construct([row[:left_size] for row in grid[:left_size]])
+        topright = self.construct([row[left_size:] for row in grid[:left_size]])
+        bottomleft = self.construct([row[:left_size] for row in grid[left_size:]])
+        bottomright = self.construct([row[left_size:] for row in grid[left_size:]])
+        return Node(1, False, topleft, topright, bottomleft, bottomright)
